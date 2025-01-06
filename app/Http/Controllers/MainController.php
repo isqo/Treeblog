002e8a4b826e9040c8a5f21e2a5f54c24f91a435
Ismael qouiqa <qouiqaismael890@gmail.com>
2025-01-06 15:31:29 +0100

    test

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -25,7 +25,7 @@ class MainController extends Controller
 
 
         $recentPostsPartitioned = Page::getRecentPagesPartitioned();
-        echo $recentPostsPartitioned;
+        print_r($recentPostsPartitioned);
         return view('pages.home', compact('currentSection', 'content', 'recentPostsPartitioned'));
     }
 
