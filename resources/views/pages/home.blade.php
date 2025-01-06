0bb89cdc0e914f078698bbe5eb45481baf0940b9
Ismael qouiqa <qouiqaismael890@gmail.com>
2025-01-06 17:31:39 +0100

    test

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -63,7 +63,7 @@
                                                 <div class="card-header">
                                                     <span class="card-title h5">       
                                                         
-                                                        @if(isset($post->section)
+                                                        @if(isset($post->section))
                                                         $section = $post->section->superSectionN(2)
                                                         @if (isset($section->name)) 	     
                                                         {{$section->name}}
@@ -73,12 +73,13 @@
                                                         </span>
                                                     -
                                                     <span class="card-title h6"> 
-                                                        @if(isset($post->section)
+                                                        @if(isset($post->section))
                                                         $section = $post->section->superSectionN(1)
                                                         @if (isset($section->name)) 	     
                                                         {{$section->name}}
                                                         @endif
                                                         @endif
+
                                                         </span>
                                                     <div class="card-title h8"><b>{{$post->title}}</b></div>
                                                 </div>
